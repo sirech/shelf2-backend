7dcc539afd786a046d7c9e93aeebc9a10ef86d60
Mario Fernandez <mario.fernandez@thoughtworks.com>
2017-10-28 11:08:14 +0200

    Make test more resilient

--- spec/lib/json_web_token_spec.rb
+++ spec/lib/json_web_token_spec.rb
@@ -4,19 +4,14 @@ require 'json_web_token'
 describe JsonWebToken, type: :model do
   let(:payload) { { user_id: 42 } }
 
-  # rubocop:disable Metrics/LineLength
-  let(:token) { 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjo0Mn0.nH_lWaJCKnuIomLHd533b4CnMryq3roBWb5ql8s9RCY' }
-
   describe '.encode' do
     subject { described_class.encode(payload) }
 
-    it { is_expected.to eq(token) }
+    it { is_expected.to be_a String }
   end
 
   describe '.decode' do
-    subject { described_class.decode(token) }
-
-    it { is_expected.to include(user_id: 42) }
+    it { expect(described_class.decode(described_class.encode(payload))).to include(user_id: 42) }
 
     it { expect(described_class.decode('duh')).to be nil }
   end
