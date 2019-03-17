d985377032fffed7a139a8f6e097959938c48842
Mario Fernandez <mario.fernandez@thoughtworks.com>
2019-03-17 12:31:17 +0100

    Update to ruby 2.6.1

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180303112039) do
+ActiveRecord::Schema.define(version: 2018_03_03_112039) do
 
-  create_table "books", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "books", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title", null: false
     t.text "description"
     t.integer "year", null: false
