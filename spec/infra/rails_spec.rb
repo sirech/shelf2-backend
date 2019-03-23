5a0a48ef255a9d5fb0e2595e99344d47233bc5dd
Mario Fernandez <mario.fernandez@thoughtworks.com>
2019-03-24 00:54:29 +0100

    fix alpine version

--- spec/infra/rails_spec.rb
+++ spec/infra/rails_spec.rb
@@ -3,7 +3,7 @@ require_relative 'infra_helper'
 describe 'Docker Image' do
   describe file('/etc/alpine-release') do
     it { is_expected.to be_file }
-    its(:content) { is_expected.to match(/3.9.0/) }
+    its(:content) { is_expected.to match(/3.9.2/) }
   end
 
   describe file('/app') do
