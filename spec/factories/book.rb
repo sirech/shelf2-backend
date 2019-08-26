a25e96a0fef0cef1a547709d98bae148b0643c44
Mario Fernandez <mario.fernandez@thoughtworks.com>
2019-08-26 20:48:19 +0200

    fix build errors

--- spec/factories/book.rb
+++ spec/factories/book.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
   factory :book do
     title { Faker::Book.title }
-    year { Faker::Number.between(2010, 2017) }
-    stars { Faker::Number.between(1, 5) }
+    year { Faker::Number.between(from: 2010, to: 2017) }
+    stars { Faker::Number.between(from: 1, to: 5) }
     category { Book.categories.keys.sample }
 
     trait :with_description do
