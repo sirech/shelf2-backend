1a42bb11d6aa315bc66208772935fe9714b1ff9c
Mario Fernandez <mario.fernandez@hceris.com>
2019-12-19 09:35:11 +0100

    fix rubocop warnings

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,7 @@ Coveralls.wear!('rails')
 
 require 'rspec/mocks'
 
-Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+Dir[Rails.root.join('spec/support/**/*.rb')].sort.each { |f| require f }
 
 RSpec.configure do |config|
   config.pattern = '**/*_spec.rb'
