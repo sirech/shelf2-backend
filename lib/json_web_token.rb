d870c5e862740f80561e30309c1aa3dac61fa340
Mario Fernandez <mario.fernandez@hceris.com>
2021-03-01 11:32:45 +0100

    fix warning

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -18,14 +18,12 @@ class JsonWebToken
     def jwks_hash
       jwks_raw = Net::HTTP.get URI('https://hceris.eu.auth0.com/.well-known/jwks.json')
       jwks_keys = Array(JSON.parse(jwks_raw)['keys'])
-      Hash[
-        jwks_keys.map do |k|
-          [
-            k['kid'],
-            OpenSSL::X509::Certificate.new(Base64.decode64(k['x5c'].first)).public_key
-          ]
-        end
-      ]
+      jwks_keys.map do |k|
+        [
+          k['kid'],
+          OpenSSL::X509::Certificate.new(Base64.decode64(k['x5c'].first)).public_key
+        ]
+      end.to_h
     end
   end
 end
